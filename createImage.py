c8c7d186f2f04d8092bb6ef2b447c8d32ff9929c
Paramecium <120632222+KanikoNayu@users.noreply.github.com>
2022-12-28 10:13:09 +0800

    Update createImage.py

--- createImage.py
+++ createImage.py
@@ -2,12 +2,14 @@ import urllib.request
 from PIL import Image
 import openai
 
-openai.api_key = "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"
+# 将自己的key填在这
+openai.api_key = ""
 
-# 设置文字，这里使用一句话描述一个图像的文字
+# 设置文字，使用一句话描述一个图像的文字，比如
 prompt = "从远处看的一辆在公路上的超级跑车"
 
 # 调用openai的text-to-image功能
+# 图片大小仅支持256x256、512x512、1024x1024
 model_engine = "image-alpha-001"
 response = openai.Image.create(
     prompt=prompt,
